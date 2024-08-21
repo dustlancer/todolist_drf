dd6ab7ed5674e8579b84a08e5f5276fd2dbd0a6b
Andrey Alperin <andreyalp@me.com>
2024-08-21 15:53:49 +0300

    readme

--- tasks/models.py
+++ tasks/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 
 class Task(models.Model):
     name = models.CharField(max_length=100)
@@ -12,8 +11,10 @@ class Task(models.Model):
     def __str__(self):
         return self.name
 
-
 class Comment(models.Model):
-    text = models.CharField(max_length=200)
     task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='comments')
+    text = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Комментарий к задаче "{self.task.title}"'
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Task, Comment
+from .serializers import TaskSerializer, CommentSerializer
 
-# Create your views here.
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    filter_backends = [filters.OrderingFilter, filters.SearchFilter]
+    ordering_fields = ['created_at', 'is_completed']
+    search_fields = ['name', 'description', 'done']
+
+    def get_queryset(self):
+            queryset = Task.objects.all()
+            is_done= self.request.query_params.get('done', None)
+            
+            if is_done is not None:
+                is_done = is_done.lower() in ['true', '1']
+                queryset = queryset.filter(done=is_done)
+            
+            return queryset
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
